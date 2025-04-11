ffc1cf771b83abdcd7d6ec10e57c42e12571418a
Aminesorati35 <Aminesorati35@gmail.com>
2025-04-11 17:43:51 +0100

    Delete Useless Files

--- frontend/src/services/api.js
+++
@@ -1,8 +0,0 @@
-// src/services/api.js
-import axios from 'axios';
-
-const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || 'http://localhost:8000/api',
-});
-
-export default api;
