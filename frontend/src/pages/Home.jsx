df3d045c98915276eaa69272da807d83c47fc65b
Aminesorati35 <Aminesorati35@gmail.com>
2025-04-11 18:20:44 +0100

    Create Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import { Navbar } from '../fixed/Navbar';
-import { Footer } from '../fixed/Footer';
-
+import React from "react";
+import { Navbar } from "../fixed/Navbar";
+import { Footer } from "../fixed/Footer";
 
 export const Home = () => {
   return (
     <div>
-        <Navbar/>
-        <div className='content' >
-            <h1>Content</h1>
+      <Navbar />
+      <div className="content">
+        <div className="hero-content">
+          <div className="hero-text">
+            <h1 className="hero-title">
+              Bienvenue sur <span className="highlight">DriveEase</span>...
+            </h1>
+            <p className="hero-description">
+              Louez ou mettez en location une voiture en toute simplicité. Que
+              vous soyez loueur ou client, trouvez ou publiez une annonce en
+              quelques clics. Commencez dès maintenant !
+            </p>
+            <div className="hero-buttons">
+              <button className="btn-primary">Espace Loueur</button>
+              <button className="btn-primary">Espace Client</button>
+            </div>
+          </div>
+          <div className="hero-image">
+            <img src="/images/cars/car1.png" alt="Car" />
+          </div>
         </div>
-        <Footer/>
+      </div>
+      <Footer />
     </div>
-  )
-}
+  );
+};
